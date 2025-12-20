d7477f1b00c2f47c0afb386b06b5680be7b7f7d3
axelfrache <axel.frache@etu.umontpellier.fr>
2025-12-20 18:13:51 +0100

    chore: adjust OpenAPI configuration indentation

--- backend/src/main/java/com/axelfrache/questify/config/OpenApiConfig.java
+++ backend/src/main/java/com/axelfrache/questify/config/OpenApiConfig.java
@@ -10,15 +10,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI questifyOpenApi() {
-        return new OpenAPI()
-                .info(
-                        new Info()
-                                .title("Questify API")
-                                .description("Gamified task management API")
-                                .version("0.0.1")
-                                .contact(new Contact().name("Axel Frache").url("https://github.com/axelfrache"))
-                                .license(new License().name("MIT")));
-    }
+  @Bean
+  public OpenAPI questifyOpenApi() {
+    return new OpenAPI()
+        .info(
+            new Info()
+                .title("Questify API")
+                .description("Gamified task management API")
+                .version("0.0.1")
+                .contact(new Contact().name("Axel Frache").url("https://github.com/axelfrache"))
+                .license(new License().name("MIT")));
+  }
 }
