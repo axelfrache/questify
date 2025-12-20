e91a63530c84cf3eda8aa025736a061ffaaeb119
axelfrache <axel.frache@etu.umontpellier.fr>
2025-12-20 17:01:21 +0100

    Initial commit

---
+++ backend/src/test/java/com/axelfrache/questify/QuestifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axelfrache.questify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
