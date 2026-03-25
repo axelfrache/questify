d7812cfabf613ab3a5a0c627ff0a4fec92db58e0
axelfrache <axel.frache@etu.umontpellier.fr>
2026-03-25 11:52:19 +0100

    feat: init quest service

---
+++ services/questify-quest-service/src/main/java/com/axelfrache/questify/quest/QuestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.axelfrache.questify.quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QuestServiceApplication.class, args);
+  }
+}
