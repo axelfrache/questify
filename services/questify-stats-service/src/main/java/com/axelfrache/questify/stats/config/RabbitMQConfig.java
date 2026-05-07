9352bbc97f603d152995778059d69cf93ddef1c5
axelfrache <axel.frache@etu.umontpellier.fr>
2026-05-07 12:15:43 +0200

    chore: replace deprecated Jackson2JsonMessageConverter with JacksonJsonMessageConverter

--- services/questify-stats-service/src/main/java/com/axelfrache/questify/stats/config/RabbitMQConfig.java
+++ services/questify-stats-service/src/main/java/com/axelfrache/questify/stats/config/RabbitMQConfig.java
@@ -8,7 +8,7 @@ import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -47,7 +47,7 @@ public class RabbitMQConfig {
 
   @Bean
   public MessageConverter messageConverter() {
-    return new Jackson2JsonMessageConverter();
+    return new JacksonJsonMessageConverter();
   }
 
   @Bean
